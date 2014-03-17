9ae4b334ebf5e1c66b5773035f332b1c7c191096
baskooiker <baskooiker90@gmail.com>
2014-03-17 15:13:30 +0100

    NetBeans project files

---
+++ nbproject/Makefile-Debug.mk
@@ -0,0 +1,108 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=Cygwin_4.x-Windows
+CND_DLIB_EXT=dll
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/1502739425/BeatHistogram.o \
+	${OBJECTDIR}/_ext/2102424670/HistogramBuffer.o \
+	${OBJECTDIR}/_ext/2102424670/Swarm.o \
+	${OBJECTDIR}/_ext/2102424670/utils.o \
+	${OBJECTDIR}/main.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/beathistogram.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/beathistogram.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/beathistogram ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/_ext/1502739425/BeatHistogram.o: ../BeatHistogram/BeatHistogram.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1502739425
+	${RM} $@.d
+	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1502739425/BeatHistogram.o ../BeatHistogram/BeatHistogram.cpp
+
+${OBJECTDIR}/_ext/2102424670/HistogramBuffer.o: ../utils/HistogramBuffer.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2102424670
+	${RM} $@.d
+	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/2102424670/HistogramBuffer.o ../utils/HistogramBuffer.cpp
+
+${OBJECTDIR}/_ext/2102424670/Swarm.o: ../utils/Swarm.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2102424670
+	${RM} $@.d
+	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/2102424670/Swarm.o ../utils/Swarm.cpp
+
+${OBJECTDIR}/_ext/2102424670/utils.o: ../utils/utils.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2102424670
+	${RM} $@.d
+	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/2102424670/utils.o ../utils/utils.cpp
+
+${OBJECTDIR}/main.o: main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -g -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/beathistogram.exe
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
